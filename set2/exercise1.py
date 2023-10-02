5b19cef77a5d1570dc04cf77de74633ea629e1e9
Jess Wang <104839715+JeYWang@users.noreply.github.com>
2023-10-02 04:52:08 +0000

    Commit up to set2 exercise 3

--- set2/exercise1.py
+++ set2/exercise1.py
@@ -11,18 +11,25 @@ import platform
 # I think this will print "hello! Let's get started" by calling the print function.
 print("hello! Let's get started")  # it printed "hello! Let's get started"
 
-some_words = ['what', 'does', 'this', 'line', 'do', '?']
+some_words = ['additional word', 'word', 'what', 'does', 'this', 'line', 'do', '?']
 
+#I think this prints "word"
 for word in some_words:
     print(word)
+    print('break')
+    #this prints each item in the list, plus 'break' which I've added to test how it works.
 
+#I think this does the same thing as the previous function 
 for x in some_words:
     print(x)
+    #this is doing exactly what the prev function does, just with a different name for the variable
 
+#this prints the list of words
 print(some_words)
 
+#if length of this string is longer than 3, it prints what's written in the brackets
 if len(some_words) > 3:
-    print('some_words contains more than 3 words')
+    print('some_words contains more than 3 words') #it printed what's in the brackets
 
 def usefulFunction():
     """
@@ -31,5 +38,6 @@ def usefulFunction():
     https://docs.python.org/3/library/platform.html#platform.uname
     """
     print(platform.uname())
+    #this identifies the current operating system, including name of OS, name of machine on the system, current release, version and hardware identifier
 
 usefulFunction()
--- set2/exercise2.py
+++ set2/exercise2.py
@@ -5,24 +5,36 @@ Go through it and change it until it runs.
 Remeber that all files must also pass the
 linter with no errors or warnings!
 """
+#linting: Linting highlights syntactical and stylistic problems in your Python source code, 
+    # which often helps you identify and correct subtle programming errors 
+    # or unconventional coding practices that can lead to errors.
 
 import string
 
-det getLetter(index):
+def getLetter(index):
     alphabet = string.ascii_lowercase + " "
-    return alphabet(index]
+    #In Python3, ascii_lowercase is a pre-initialized string used as string constant. 
+    # In Python, string ascii_lowercase will give the lowercase letters ‘abcdefghijklmnopqrstuvwxyz’.
+    return alphabet[index]
+#index is where the item sits inside a list
+# [] is the only way to make python recognise your number is an INDEX and not a STRING
 
 
-def set2exersise2();
-    indices = [12: 2, 26, 7, 0, 12, 12, 4, 17]
-    wordArray = map(getLetter, indices)
+def set2exercise2():
+    indices = [12, 2, 26, 7, 0, 12, 12, 4, 17]
+    wordArray = [getLetter(x) for x in indices]
+    #map runs the function for each iterator/item, as defined by a previous function
     wordArray[0] = wordArray[0].upper()
-    wordArray{1} = wordArray[1].upper()
-    wordArray[3} = wordArray[3].upper{}
+    wordArray[1] = wordArray[1].upper()
+    wordArray[3]= wordArray[3].upper()
     secret_word="".join(wordArray)
+    # the "" defines what goes between strings as they're being joined
     print(secret_word)
     return secret_word
+    #return command tells the computer that this is the end result, whereas print just prints it without associations
 
 
-if __name__ = = "__main__":
-    prin(set2exersise2())
+if __name__ == "__main__":
+    hero = set2exercise2()
+    print(hero)
+    #one = is a forumla, and == is comparing left and right sides
